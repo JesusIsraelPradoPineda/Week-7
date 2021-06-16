7c2203c3fce897d6d9f12e85fa8156d8406c0a45
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-06-16 08:02:21 -0500

    this is a commit

---
+++ Chapter6ex06.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+char at[100];
+
+int main(void) {
+  float x1;
+  float extra;
+  printf("give the hours worked \n");
+  scanf("%f",&x1);
+  if (x1 > 40)
+  {
+    extra = x1-40;
+    printf("he should to be payed with %fX where X is the salary that the worker recives",extra*1.5+x1);
+  }
+  else {
+    printf("he should to be payed with %fX where X is the salary that the worker recives",x1);
+  
+  }
+}
