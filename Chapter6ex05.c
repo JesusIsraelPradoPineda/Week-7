b997dadea1e6895167f126b79519d03df0162f84
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-06-16 07:55:24 -0500

    this is a commit

---
+++ Chapter6ex05.c
@@ -0,0 +1,44 @@
+  
+/********************************************************
+ * Ex06 -- program to print out if a year is leap or not. *
+ * Not an especially earth-shattering program. *
+ * *
+ * Author: Israel Prado. *
+ * *
+ * Purpose: know if the year is leap or not. *
+ * *
+ * Usage: *
+ * put the year in the program and it will say if this is leap or not. *
+ ********************************************************/
+
+#include <stdlib.h>
+#include <stdio.h> // input output data
+#include <math.h> 
+#include <string.h> // string handling operations
+#define MAX 1000
+
+int main(void) {
+  int a1 ; //I defined the variable type integer because we do not have fraction years :(
+
+  printf("what year do you want know?\n"); //I ask the year to analizate
+  scanf("%d",&a1);  // read from prompt
+  if (a1 % 400 == 0){ //If it is a multiple of 400, it will be 
+    printf ("%d",a1);
+    printf (" is the year and therefore is bisiesto");
+  }
+  else if (a1 % 100 == 0 ) { //else if it is multiple of 100, it will not be a leap year
+    printf ("%d",a1);
+    printf (" is the year and therefore is not bisiesto");
+  }
+  else if (a1 % 4 == 0) { //else if it is multiple of 4, it will be
+    printf ("%d",a1);
+    printf (" is the year and therefore is bisiesto");
+  }
+  else {
+    printf ("%d",a1); //if i dont have any condition, it will not be 
+    printf (" is the year and therefore is not bisiesto");
+  }
+  return 0;
+
+
+}
