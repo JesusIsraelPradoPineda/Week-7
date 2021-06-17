cfaabf40f956ec089ad3c8c5d88a8d4979f3ddb1
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-06-16 19:19:44 -0500

    this is a commit

--- Chapter7ex04.c
+++ Chapter7ex04.c
@@ -1,12 +1,23 @@
+/********************************************************
+ * Ex01 -- redondeo. *
+ * Not an especially earth-shattering program. *
+ * *
+ * Author: Israel Prado. *
+ * *
+ * Purpose: saber usar el redondeo en C. *
+ * *
+ * Usage: *
+ * pon la cantidad y postriormente te dara la cantidad al centavo mas cercano. *
+ ********************************************************/
 #include <stdio.h>
 #include <math.h>
 
 int main(void) {
-  float sell;
-  float res;
-  printf("cuanto vendiste el dia de hoy?\n");
+  float sell; //esta variable es la que se va a guardar
+  float res; //esta es la variable de impuesto
+  printf("cuanto vendiste el dia de hoy?\n");//ingresa y registra la variable
   scanf("%f",&sell);
-  res= (sell*5)/100;
-  printf("you need to pay %0.2f of tax due you won %0.2f aprox\n",res,sell);
+  res= (sell*5)/100;//se realiza la operacion para poder definir el impuesto
+  printf("you need to pay %0.2f of tax due you won %0.2f aprox\n",res,sell);//muestro los resultados redondeados a dos decimas
   return 0;
 }
