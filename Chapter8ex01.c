b882da13c6a6d55adeb52821ba1c8ab66e0225d8
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-06-20 14:02:54 -0500

    Chapter 08  Excersise 01 finished

--- Chapter8ex01.c
+++ Chapter8ex01.c
@@ -1,4 +1,14 @@
-
+/********************************************************
+ * Ex01 -- the square. *
+ * Not an especially earth-shattering program. *
+ * *
+ * Author: Israel Prado. *
+ * *
+ * Purpose: el uso del for muchas veces par generar cuadrados diseñados por el usuario. *
+ * *
+ * Usage: *
+ * pon cuantos cuadros en alto quieres y en ancho lo mismo. *
+ ********************************************************/
 #include <stdio.h>
 int main(void) {
   int xd;
@@ -6,9 +16,9 @@ int main(void) {
   printf("cuantos quieres ver rey para la derecha\n");
   scanf("%d",&xd);
   printf("y a pa abajo? \n");
-  scanf("%d",&abajo);
-  for (int e=0 ; e <abajo; e++){
-  for (int i=0 ; i <xd;i++){
+  scanf("%d",&abajo); //pido las dimensiones de este cuadrado
+  for (int e=0 ; e <abajo; e++){ //antes de poner aabajo, los pondre a la derecha
+  for (int i=0 ; i <xd;i++){ 
   printf("+-----");
   }
   printf("+");
@@ -25,12 +35,12 @@ int main(void) {
   printf("\n");
   printf("|");
   for (int i=0 ; i <xd;i++){
-  printf("    |");
+  printf("    |"); //a partir de las especificaciones, se repite varias veces el mismo codigo para subir y bajar el nivel de cuadritos dependeiendo de lo que se necesite
   }
   printf("\n");
   }
    for (int i=0 ; i <xd;i++){
-  printf("+-----");
+  printf("+-----"); //se define solo lo mismo pero ahora crecera hacia abajo
   }
 return 0;
 }
