16958ac8d7a8b002442186d6a269e27cfbef26d1
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-06-20 15:12:08 -0500

    finished chapter8 ex 05

--- Chapter8ex05.c
+++ Chapter8ex05.c
@@ -1,23 +1,23 @@
 /********************************************************
- * Ex03 -- Upload time. *
+ * Ex03 -- Consonant or Voocal?. *
  * Not an especially earth-shattering program. *
  * *
  * Author: Israel Prado. *
  * *
- * Purpose: the use of some functions. *
+ * Purpose: the ascii code is the most important and we are going to study the characters of vocals. *
  * *
  * Usage: *
- * put a value in bytes and it will show the upload time in hours and days. *
+ * put a valid value and then you will show the diferrence. *
  ********************************************************/
 #include <stdio.h>
 int main(void) {
 
   int xd;
   printf("el codigo ascii es un codigo maquina que utilizan las computadoras utilizan para decodificar de letras a numeros binarios, entonces, las letras mayusculas corresponden del 65 hasta el 90 y las minusculas del 97 al 122, pon un numero dentro de esos rango y te dire si es consonante o vocal \n");
-  scanf("%d",&xd);
-  while ((xd >= 65 && xd <=90)||(xd >=95 && xd <= 122)) {
+  scanf("%d",&xd);// doy informacion y luego pido informacion
+  while ((xd >= 65 && xd <=90)||(xd >=95 && xd <= 122)) { //mientras el numero que introduzcas este en esos rangos entras a la funcion, de lo contrario sales
     if (xd == 65 || xd== 69 || xd == 73 || xd==79 || xd==85 ||xd == 65 || xd== 69 || xd == 73 || xd==79 || xd==85) {
-    printf("es vocal\n");
+    printf("es vocal\n"); //si se cumple la condicion del codigo muestra vocal, de lo contrario es consonante
     printf("dame otro numero \n");
      scanf("%d",&xd);
     }
