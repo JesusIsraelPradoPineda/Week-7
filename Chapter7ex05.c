1492ea52c211b276e1c605f5d4e61442c5515962
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-06-20 13:18:32 -0500

    Chapter ex 05 commented finished

--- Chapter7ex05.c
+++ Chapter7ex05.c
@@ -9,25 +9,25 @@
  * Usage: *
  * ingresa el numero y el programa te dira si es o no. */
 #include <stdio.h>
-int verdad(int numero); //se definen las variables de entrada y salida
+int verdad(int xd); //se definen las variables de entrada y salida
 
 int main(void) {
-  int numero; //defino la variable numero
+  int xd; //defino la variable numero
   printf("Dime un número: \n"); //pido el numero y se guarda
-  scanf("%d", &numero);
-  if (verdad(numero)) {
-    printf("Es primo");
+  scanf("%d", &xd); 
+  if (verdad(xd)) {
+    printf("Es primo"); //se define como primo a numero que solo se puede dividir entre 1 y 0 sin dejar decimales
   } else {
-    printf("No es primo");
+    printf("No es primo"); //estos se pueden dividir con otros numero sin dejar decimales
   }
   return 0;
 }
 
-int verdad(int numero) {
-  if (numero == 0 || numero == 1) return 0;
-  if (numero == 4) return 0;
-  for (int x = 2; x < numero / 2; x++) {
-    if (numero % x == 0) return 0;
+int verdad(int xd) {
+  if (xd == 0 || xd 1) return 0; //se cumple la condicion y regresa al return 0
+  if (xd == 4) return 0; //se cumple esta condicion y regresamos a retunr 0
+  for (int x = 2; x < xd / 2; x++) {
+    if (xd % x == 0) return 0; //se cumple la condicion y regresa a la variable de arriba
   }
   return 1;
 }
