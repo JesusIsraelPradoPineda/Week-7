96e26947771a1ebb3ff8d4a1247e2ecf40e8a473
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-06-20 13:25:18 -0500

    chapter 8 ex 03 finished

--- Chapter8ex03.c
+++ Chapter8ex03.c
@@ -1,24 +1,35 @@
+/********************************************************
+ * Ex03 -- numbers average. *
+ * Not an especially earth-shattering program. *
+ * *
+ * Author: Israel Prado. *
+ * *
+ * Purpose: use a for in order to get an averge of n numbers. *
+ * *
+ * Usage: *
+ * put the number of numbers to get the average. *
+ ********************************************************/
 #include "stdio.h"
 int main(void) {
   int xd;
   int i;
   float num;
   float res = 0;
-  float resfin;
+  float resfin; //I have the variables
   printf("cuantos numeros vamos a promediar?\n");
   scanf("%d",&xd);
-  for (i = 1; i<=xd;i++){
+  for (i = 1; i<=xd;i++){ //I get in a for then i asked how much numbers do we need
     printf("\n");
     printf("dame numero %d \n",i);
     scanf("%f",&num);
-    res=res+num;
-    if (i==xd){
+    res=res+num; //I ask the numbers
+    if (i==xd){ 
       break;
     }
-    printf("el numero %d de esta cadena de operaciones es %0.2f\n",i,num);
+    printf("el numero %d de esta cadena de operaciones es %0.2f\n",i,num); //I show that in screen
    }
    resfin=res/xd;
    
-    printf("el numero %d fue %0.2f, el promedio de todos estos es %0.2f",i,num,resfin);
+    printf("el numero %d fue %0.2f, el promedio de todos estos es %0.2f",i,num,resfin); //I show the average 
   return 0;
 }
