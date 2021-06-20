0fce81f8b8766295f2f689491769b117440b9377
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-06-20 13:22:20 -0500

    finished chapter 8 ex 04

--- Chapter8ex04.c
+++ Chapter8ex04.c
@@ -1,14 +1,25 @@
+/********************************************************
+ * Ex04 -- tablas de multiplicar. *
+ * Not an especially earth-shattering program. *
+ * *
+ * Author: Israel Prado. *
+ * *
+ * Purpose: ver las tablas de multiplicar. *
+ * *
+ * Usage: *
+ * follow thw instructions. *
+ ********************************************************/
 #include "stdio.h"
 int main(void) {
  int xd;
  int xdx;
- int i;
- printf("que tabla quieres saber?\n");
+ int i; //defino variables
+ printf("que tabla quieres saber?\n"); 
  scanf("%d",&xd);  
  printf("cuantos valores quieres ver en as tablas?\n");
  scanf("%d",&xdx);
  for (i=1;i<=xdx;i++){
-   printf("%d x %d == %d \n",xd,i,xd*i);
+   printf("%d x %d == %d \n",xd,i,xd*i); //segun el numero de tablas que se quieran visualizar asi como el numero que va a ser multipliccado, se pone en el programa
  }
  return 0;
 }
