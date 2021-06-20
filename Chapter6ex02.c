55462269dc85f2d061f9c49c6a0f387f52420505
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-06-20 13:32:51 -0500

    Commented ex 02 chapter 6

--- Chapter6ex02.c
+++ Chapter6ex02.c
@@ -1,12 +1,23 @@
+/********************************************************
+ * Ex02 -- grades. *
+ * Not an especially earth-shattering program. *
+ * *
+ * Author: Israel Prado. *
+ * *
+ * Purpose: put some letters when you get a grade. *
+ * *
+ * Usage: *
+ * put 3 units and then you'll see the letter. *
+ ********************************************************/
 #include <stdio.h>
 #include <math.h>
-char at[100];
+char at[100]; // I'll do a string
 
 int main(void) {
   float x1,x2,x3,res;
-  printf("resultado final de 3 unidades \n");
+  printf("resultado final de 3 unidades \n"); 
   fgets(at, sizeof(at), stdin);
-  sscanf(at, "%f %f %f \n", &x1,&x2,&x3);
+  sscanf(at, "%f %f %f \n", &x1,&x2,&x3); //I ask the numbers as a b c form, because i am in a string
   res = (x1+x2+x3)/3;
   printf("promedio es %f \n",res);
 
@@ -22,7 +33,7 @@ int main(void) {
   else if (res>81 && res<90.9999999999) {
    printf ("B\n");
  }
-  else if (res>91 && res<100) {
+  else if (res>91 && res<100) { //I put the conditions if the average of the numbers is equal of some number, and i put taht in a differwent form
    printf ("A\n");
  }
 }
