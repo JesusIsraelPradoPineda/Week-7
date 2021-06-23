6fecf34e7f17cac43124746b03493935c074d7f2
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-06-23 08:36:52 -0500

    Chapter 7 ex 06 finished

--- Chapter7ex06.c
+++ Chapter7ex06.c
@@ -1,3 +1,14 @@
+/********************************************************
+ * Ex06 -- negative, positives and 0 count. *
+ * Not an especially earth-shattering program. *
+ * *
+ * Author: Israel Prado. *
+ * *
+ * Purpose: pracitce the sum of the positives numbers, negatives and 0 and then show it in screen. *
+ * *
+ * Usage: *
+ * put an amount of numbers and then the numbers. *
+ ********************************************************/
 #include <stdio.h>
 
 int main(void) {
@@ -6,22 +17,22 @@ int main(void) {
   int n = 0;
   int p = 0;
   int o = 0;
-  char at[1000];
-  printf("cuantos numeros imprimimos?\n");
- fgets(at, sizeof(at), stdin);
+  char at[1000]; //se definen las variables a usar en el programa
+  printf("cuantos numeros imprimimos?\n"); 
+ fgets(at, sizeof(at), stdin); //pido la cantidad de numeros y guardo el resultado
   sscanf(at,"%d",&a);
   for (int i=1;i<=a;i++)
   {
-  printf("dame numeros\n");
+  printf("dame numero %d\n",i);
   fgets(at, sizeof(at), stdin);
-  sscanf(at,"%d",&b);
-  if(b < 0){
+  sscanf(at,"%d",&b); // pido que me de los numeros
+  if(b < 0){ //si es menor que 0, se agrega a la cuenta de los negativos
     n=n+1;
   }
-  else if (b > 0){
+  else if (b > 0){  //si es mayor de cer0, se agrega a la cuenta de los positivos
     p=p+1;
   }
-  else if( b == 0){
+  else if( b == 0){ //si es cero, se cuenta dentro los ceros
     o=o+1;
 
   }
