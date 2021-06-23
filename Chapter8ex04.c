6ab91ca18488294fe506cb389999bdffcb5a285a
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-06-23 08:56:02 -0500

    Chapter 8 ex 04 Finished

--- Chapter8ex04.c
+++ Chapter8ex04.c
@@ -14,10 +14,10 @@ int main(void) {
  int xd;
  int xdx;
  int i; //defino variables
- printf("que tabla quieres saber?\n"); 
- scanf("%d",&xd);  
- printf("cuantos valores quieres ver en as tablas?\n");
- scanf("%d",&xdx);
+ char at[1000];
+ printf("que tabla quieres saber y cuantos valores quieres ver?\n"); 
+fgets(at, sizeof(at), stdin);
+  sscanf(at,"%d %d",&xd,&xdx);
  for (i=1;i<=xdx;i++){
    printf("%d x %d == %d \n",xd,i,xd*i); //segun el numero de tablas que se quieran visualizar asi como el numero que va a ser multipliccado, se pone en el programa
  }
