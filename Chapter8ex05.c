06242352b7a856a4981777e348311fef1857fc78
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-06-23 08:58:18 -0500

    Chapter 8 Ex 05 finished

--- Chapter8ex05.c
+++ Chapter8ex05.c
@@ -13,18 +13,22 @@
 int main(void) {
 
   int xd;
-  printf("el codigo ascii es un codigo maquina que utilizan las computadoras utilizan para decodificar de letras a numeros binarios, entonces, las letras mayusculas corresponden del 65 hasta el 90 y las minusculas del 97 al 122, pon un numero dentro de esos rango y te dire si es consonante o vocal \n");
-  scanf("%d",&xd);// doy informacion y luego pido informacion
+  char at[1000];
+  printf("el codigo ascii es un codigo maquina que utilizan las computadoras para decodificar de letras a numeros binarios, entonces, las letras mayusculas corresponden del 65 hasta el 90 y las minusculas del 97 al 122, pon un numero dentro de esos rango y te dire si es consonante o vocal \n");
+   fgets(at, sizeof(at), stdin);
+  sscanf(at,"%d",&xd);// doy informacion y luego pido informacion
   while ((xd >= 65 && xd <=90)||(xd >=95 && xd <= 122)) { //mientras el numero que introduzcas este en esos rangos entras a la funcion, de lo contrario sales
     if (xd == 65 || xd== 69 || xd == 73 || xd==79 || xd==85 ||xd == 65 || xd== 69 || xd == 73 || xd==79 || xd==85) {
     printf("es vocal\n"); //si se cumple la condicion del codigo muestra vocal, de lo contrario es consonante
     printf("dame otro numero \n");
-     scanf("%d",&xd);
+       fgets(at, sizeof(at), stdin);
+  sscanf(at,"%d",&xd);
     }
     else {
       printf("es consonante\n");
       printf("dame otro numero \n");
-     scanf("%d",&xd);
+       fgets(at, sizeof(at), stdin);
+     sscanf(at,"%d",&xd);
     }
   }
   printf("no es un valor valido");
